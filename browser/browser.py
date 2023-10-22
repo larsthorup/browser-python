7c92956a0a4c3efd8e8dface27637a77b26915e3
Lars Thorup <lars@zealake.com>
2023-10-22 06:22:07 +0200

    render in window

--- browser/browser.py
+++ browser/browser.py
@@ -1,9 +1,31 @@
 import sys
+import tkinter
 from url import URL
 
+WIDTH, HEIGHT = 800, 600
 
-def show(body: str):
+
+class Browser:
+    def __init__(self):
+        self.window = tkinter.Tk()
+        self.canvas = tkinter.Canvas(self.window, width=WIDTH, height=HEIGHT)
+        self.canvas.pack()
+
+    def load(self, url):
+        _, body = url.request()
+        text = lex(body)
+        HSTEP, VSTEP = 13, 18
+        cursor_x, cursor_y = HSTEP, VSTEP
+        for c in text:
+            self.canvas.create_text(cursor_x, cursor_y, text=c)
+            cursor_x += HSTEP
+            if cursor_x >= WIDTH - HSTEP:
+                cursor_y += VSTEP
+                cursor_x = HSTEP
+
+def lex(body: str):
     # TODO: handle entities
+    text = ""
     in_angle = False
     for c in body:
         if c == "<":
@@ -11,13 +33,12 @@ def show(body: str):
         elif c == ">":
             in_angle = False
         elif not in_angle:
-            print(c, end="")
-
-
-def load(url):
-    headers, body = url.request()
-    show(body)
+            text += c
+    return text
 
 
 if __name__ == "__main__":
-    load(URL(sys.argv[1]))
+    url = URL(sys.argv[1])
+    browser = Browser()
+    browser.load(url)
+    tkinter.mainloop()
