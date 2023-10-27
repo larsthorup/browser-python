07ed9db5ebc0a2bfcab44dae4b935dfa3d509f38
Lars Thorup <lars@zealake.com>
2023-10-27 16:52:35 +0200

    clean code

--- browser/lexer.py
+++ browser/lexer.py
@@ -1,11 +1,18 @@
 class Token:
     pass
 
+
 class Text(Token):
     text: str
 
     def __init__(self, text: str):
         self.text = text
+
+    def __repr__(self):
+        return f'{__class__.__name__ }("{self.text}")'  # type: ignore[name-defined] # https://github.com/python/mypy/issues/4177
+
+    def __eq__(self, other: object):
+        return isinstance(other, __class__) and self.text == other.text  # type: ignore[name-defined] # https://github.com/python/mypy/issues/4177
 
 
 class Tag(Token):
@@ -13,6 +20,12 @@ class Tag(Token):
 
     def __init__(self, tag: str):
         self.tag = tag
+
+    def __repr__(self):
+        return f'{__class__.__name__ }("{self.tag}")'  # type: ignore [name-defined] # https://github.com/python/mypy/issues/4177
+
+    def __eq__(self, other: object):
+        return isinstance(other, __class__) and self.tag == other.tag  # type: ignore [name-defined] # https://github.com/python/mypy/issues/4177
 
 
 def lex(body: str):
